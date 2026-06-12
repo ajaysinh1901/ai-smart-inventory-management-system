3ded355f553245c729b20062e07b3978bb1ba9e3
PARMAR AJAYSINH MANGALSINH <ajaysinhzt@gmail.com>
2026-06-12 13:46:03 +0530

    Delete tests directory

--- tests/bugs/phase2/check-admin.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/MERNDB').then(async () => {
-  const users = await mongoose.connection.db.collection('users').find({}).project({email:1,role:1,name:1,_id:0}).toArray();
-  console.log('All users:', JSON.stringify(users, null, 2));
-  process.exit(0);
-}).catch(e => { console.error(e.message); process.exit(1); });
